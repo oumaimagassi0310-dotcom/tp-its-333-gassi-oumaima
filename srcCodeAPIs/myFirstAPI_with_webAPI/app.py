24faea383b3e0308cb46ebc94ec83fd173631589
Oumaima <oumaima@gmail.com>
2026-01-12 16:25:00 +0100

    Premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -1,7 +1,3 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
 
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
@@ -9,6 +5,17 @@ app = Flask(__name__)
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
 # >> curl -i -X POST -H 'Content-Type: application/json' -d '{"nom": "Bob"}' http://localhost:5000/api/utilisateurs
+
+
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# EXO1: API GET
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
+    return jsonify({"message": "Hello World"})
 
 if __name__ == '__main__':
     app.run(debug=True)
