2839b749937f6d31ef44db716c6c141cb3a07daf
Oumaima <oumaima@gmail.com>
2026-01-12 16:29:21 +0100

    premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -13,9 +13,21 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # EXO1: API GET
-@app.route('/api/salutation', methods=['GET'])
-def salutation():
-    return jsonify({"message": "Hello World"})
+#@app.route('/api/salutation', methods=['GET'])
+#def salutation():
+    #return jsonify({"message": "Hello World"})
+
+
+
+# EXO2: API POST
+@app.route('/api/utilisateurs', methods=['POST'])
+def utilisateurs():
+    data = request.get_json()
+
+    if not data or 'nom' not in data:
+        return jsonify({"error": "Le champ 'nom' est requis"}), 400
+
+    return jsonify({"nom": data['nom']}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
