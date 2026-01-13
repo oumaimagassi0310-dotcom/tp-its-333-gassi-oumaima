91e3dd8147cb27896d7a4f2123c35815fc4bad4b
Oumaima <oumaima@gmail.com>
2026-01-13 09:47:15 +0100

    ex 1 à 4 (sans commentaire)

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,30 +2,30 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
-#@app.route('/api/salutation', methods=['GET'])
-#def salutation():
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
     #return jsonify({"message": "Hello World"})
 
 
 ### EXO2 - API with simple display
-#@app.route('/')
-#def index():
-    #message = "Hello world"
-    #return render_template ('index.html', message=message)
+@app.route('/route2')
+def index2():
+    message = "Hello world"
+    return render_template ('index.html', message=message)
 
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 ### EXO3 - API with parameters display
-#@app.route('/')
-#def index():
-    #user={'name':'Oumaima', 'surname':'gassi'}
-    #return render_template('index.html', title='MDM', utilisateur=user) 
+@app.route('/route3')
+def index3():
+    user={'name':'Oumaima', 'surname':'gassi'}
+    return render_template('index.html', title='MDM', utilisateur=user) 
 
 ### EXO4 - API with parameters retrieved from URL 
-@app.route('/')
-def index():
+@app.route('/route4')
+def index4():
     name = request.args.get('name', 'Oumaima')
     surname = request.args.get('surname', 'Gassi')
     user = {'name': name, 'surname':surname}
